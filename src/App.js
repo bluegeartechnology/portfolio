dce4cefe409fb5942632d2563d8500e0455cc45b
Josh <bluegeartechnology@gmail.com>
2020-11-04 14:38:37 -0500

    Renamed .contactButton to .contactLink.

--- src/App.js
+++ src/App.js
@@ -217,14 +217,14 @@ function App() {
 
           <div className="d-flex row justify-content-center my-5 ">
 
-            <div target='blank' className='contactButton text-center text-primary mx-2'>
+            <div target='blank' className='contactLink text-center text-primary mx-2'>
               <a href='mailto:joshc@bluegeartechnology.com' className="mx-auto btn text-primary">
                 <i className="fa fa-envelope fa-5x"></i>
               </a>
               <h3 id='email' >joshc@bluegeartechnology.com</h3>
             </div>
 
-            <a href='/resume' target='blank' className='contactButton text-success text-center btn mx-2'>
+            <a href='/resume' target='blank' className='contactLink text-success text-center btn mx-2'>
               <i className="fa fa-file fa-5x mx-auto"></i>
               <h3 id='github' className='text-center' >Résumé</h3>
             </a>
@@ -238,17 +238,17 @@ function App() {
 
 
 
-            <a href='https://github.com/bluegeartechnology' target='blank' className='contactButton text-center text-dark btn col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href='https://github.com/bluegeartechnology' target='blank' className='contactLink text-center text-dark btn col-12 col-sm-6 col-md-4 col-lg-3'>
               <i className="fa fa-github fa-5x mx-auto"></i>
               <h3 id='github' className='text-center' >GitHub</h3>
             </a>
 
-            <a href='https://youtube.com/' target='blank' className='contactButton text-center text-primary btn col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href='https://youtube.com/' target='blank' className='contactLink text-center text-primary btn col-12 col-sm-6 col-md-4 col-lg-3'>
               <i className="fa fa-linkedin-square fa-5x mx-auto"></i>
               <h3 id='youtube' >LinkedIn</h3>
             </a>
 
-            <a href='https://youtube.com/' target='blank' className='contactButton text-center text-danger btn col-12 col-sm-6 col-md-4 col-lg-3'>
+            <a href='https://youtube.com/' target='blank' className='contactLink text-center text-danger btn col-12 col-sm-6 col-md-4 col-lg-3'>
               <i className="fa fa-youtube-play fa-5x mx-auto"></i>
               <h3 id='youtube' >YouTube</h3>
             </a>
