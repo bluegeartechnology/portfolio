0a13040be54f66c946bfc6a939e60d3f65b909a8
Josh <bluegeartechnology@gmail.com>
2020-11-06 09:45:59 -0500

    Bootstrap skill icon had .hvr-bob class on both the div that contains the image/skill name (correct) , and the image itself (incorrect), resuling in improper animation.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         {/*<img src={logo} className="App-logo" alt="logo" /> */}
 
         <div id='headerJumbotron' className="jumbotron container mx-auto align-content-middle text-center overflow-hidden row d-flex justify-content-around">
-          <div id="headshotContainer" className='col-lg-3 col-md-5 col-sm-4 col-4 animate__animated animate__fadeIn'>
+          <div id="headshotContainer" className='col-lg-3 col-md-5 col-sm-6 col-6 animate__animated animate__fadeIn'>
             <img id='headshot' className='rounded' src="./PictureSquared.jpg" alt="" />
           </div>
 
@@ -234,7 +234,7 @@ function App() {
 
 
               <div className='col-lg-2 col-sm-5 col-6 hvr-bob'>
-                <svg width="" height="100" viewBox="0 0 16 16" class="col-12 bi bi-bootstrap-fill hvr-bob" fill="#7952b3" xmlns="http://www.w3.org/2000/svg">
+                <svg width="" height="100" viewBox="0 0 16 16" class="col-12 bi bi-bootstrap-fill" fill="#7952b3" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z" />
                 </svg>
                 <h3 className='text-center text-dark'>Bootstrap</h3>
