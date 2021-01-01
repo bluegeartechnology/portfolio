295f62b56aa7a11a91860bf3ba3f8d73a7f5158e
Josh <bluegeartechnology@gmail.com>
2020-12-31 21:26:35 -0500

    Added placeholder portfolio project cards for hypothetical projects that I may make in the future.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -115,23 +115,135 @@ const Portfolio = () => {
 
 
 
-                    <div className="portfolioProjectCard btn btn-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 ">
-                        <h3>ACCESS</h3>
+
+                    <div className="portfolioProjectCard btn bg-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 " onClick={() => {
+                        history.push('/tomatokei')
+                    }}>
+                        <div>
+                            <div className="my-3 rounded projectImageDiv align-middle d-flex justify-content-center">
+                                <img src="\tomatokei.png" alt="" className='rounded my-auto' onLoad={(e) => e.target.classList.add('animate__animated', 'animate__fadeIn')} />
+                            </div>
+                            <div className='mx-4'>
+                                <h3>CookDown</h3>
+                                <small>Calculates food mass loss from cooking.</small>
+                                <br />
+                                <br />
+                                <p>Food changes mass significantly after being cooked. This can throw calorie-counting measurements way out of whack.</p>
+                                <p>Knowing the difference is extremely useful for both bodybuilding and general weight management.</p>
+                            </div>
+
+                            <div className="d-flex justify-content-center d-flex p-2">
+                                <a href="https://tomatokei.herokuapp.com" target='blank'><button className="btn btn-primary mx-1 my-1"><i className="fa fa-globe"></i>&nbsp;Visit</button></a>
+                                <Link to="./tomatokei" ><button className="btn btn-success mx-1 my-1"><i className="fa fa-bar-chart"></i>&nbsp;Details</button></Link>
+                                <a href="https://github.com/bluegeartechnology/Tomatokei" target='blank'><button className="btn btn-secondary mx-1 my-1"><i className="fa fa-github"></i>&nbsp;GitHub</button></a>
+                            </div>
+
+                        </div>
                     </div>
 
 
 
+                    <div className="portfolioProjectCard btn bg-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 " onClick={() => {
+                        history.push('/tomatokei')
+                    }}>
+                        <div>
+                            <div className="my-3 rounded projectImageDiv align-middle d-flex justify-content-center">
+                                <img src="\tomatokei.png" alt="" className='rounded my-auto' onLoad={(e) => e.target.classList.add('animate__animated', 'animate__fadeIn')} />
+                            </div>
+                            <div className='mx-4'>
+                                <h3>BarBella</h3>
+                                <small>Barbell weight combination calculator</small>
+                                <br />
+                                <br />
+                                <p>App to calculate the possible combinations of weights that could be added to a barbell to reach a given weight.</p>
+                                <p></p>
+                            </div>
 
-                    <div className="portfolioProjectCard btn btn-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 ">
-                        <div className="my-3 rounded projectImageDiv align-middle">
-                            <img src="\tomatokei.png" alt="" className='rounded my-auto' />
+                            <div className="d-flex justify-content-center d-flex p-2">
+                                <a href="https://tomatokei.herokuapp.com" target='blank'><button className="btn btn-primary mx-1 my-1"><i className="fa fa-globe"></i>&nbsp;Visit</button></a>
+                                <Link to="./tomatokei" ><button className="btn btn-success mx-1 my-1"><i className="fa fa-bar-chart"></i>&nbsp;Details</button></Link>
+                                <a href="https://github.com/bluegeartechnology/Tomatokei" target='blank'><button className="btn btn-secondary mx-1 my-1"><i className="fa fa-github"></i>&nbsp;GitHub</button></a>
+                            </div>
+
                         </div>
-                        <h3>Tomatokei</h3>
-                        <small>A Pomodoro Clock and Time Management Tool</small>
-                        <br />
-                        <br />
-                        <p>Designed to help manage ADHD symptoms and stay on task.</p>
-                        <p>Integrated with Google Calendar for optimal ease of use.</p>
+                    </div>
+
+
+                    <div className="portfolioProjectCard btn bg-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 " onClick={() => {
+                        history.push('/tomatokei')
+                    }}>
+                        <div>
+                            <div className="my-3 rounded projectImageDiv align-middle d-flex justify-content-center">
+                                <img src="\tomatokei.png" alt="" className='rounded my-auto' onLoad={(e) => e.target.classList.add('animate__animated', 'animate__fadeIn')} />
+                            </div>
+                            <div className='mx-4'>
+                                <h3>OhSee</h3>
+                                <small>An app to alleviate checking-type OCD symptoms.</small>
+                                <br />
+                                <br />
+                                <p>Helps the user to keep track of the things they compulsively check, and prove to themselves that all is well.</p>
+                                <p>"Oh, see? I did remember it after all."</p>
+                            </div>
+
+                            <div className="d-flex justify-content-center d-flex p-2">
+                                <a href="https://tomatokei.herokuapp.com" target='blank'><button className="btn btn-primary mx-1 my-1"><i className="fa fa-globe"></i>&nbsp;Visit</button></a>
+                                <Link to="./tomatokei" ><button className="btn btn-success mx-1 my-1"><i className="fa fa-bar-chart"></i>&nbsp;Details</button></Link>
+                                <a href="https://github.com/bluegeartechnology/Tomatokei" target='blank'><button className="btn btn-secondary mx-1 my-1"><i className="fa fa-github"></i>&nbsp;GitHub</button></a>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="portfolioProjectCard btn bg-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 " onClick={() => {
+                        history.push('/tomatokei')
+                    }}>
+                        <div>
+                            <div className="my-3 rounded projectImageDiv align-middle d-flex justify-content-center">
+                                <img src="\tomatokei.png" alt="" className='rounded my-auto' onLoad={(e) => e.target.classList.add('animate__animated', 'animate__fadeIn')} />
+                            </div>
+                            <div className='mx-4'>
+                                <h3>Lifter</h3>
+                                <small>An app for keeping track of weightlifting progression.</small>
+                                <br />
+                                <br />
+                                <p>Takes the guesswork out of progression by calculating weights based on the user's goals, stats, and other inputs.</p>
+                                <p></p>
+                            </div>
+
+                            <div className="d-flex justify-content-center d-flex p-2">
+                                <a href="https://tomatokei.herokuapp.com" target='blank'><button className="btn btn-primary mx-1 my-1"><i className="fa fa-globe"></i>&nbsp;Visit</button></a>
+                                <Link to="./tomatokei" ><button className="btn btn-success mx-1 my-1"><i className="fa fa-bar-chart"></i>&nbsp;Details</button></Link>
+                                <a href="https://github.com/bluegeartechnology/Tomatokei" target='blank'><button className="btn btn-secondary mx-1 my-1"><i className="fa fa-github"></i>&nbsp;GitHub</button></a>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className="portfolioProjectCard btn bg-light col-11 col-sm-11 col-md-5 col-lg-3 my-2 mx-1 " onClick={() => {
+                        history.push('/tomatokei')
+                    }}>
+                        <div>
+                            <div className="my-3 rounded projectImageDiv align-middle d-flex justify-content-center">
+                                <img src="\tomatokei.png" alt="" className='rounded my-auto' onLoad={(e) => e.target.classList.add('animate__animated', 'animate__fadeIn')} />
+                            </div>
+                            <div className='mx-4'>
+                                <h3>ReciPeace</h3>
+                                <small>Scales recipies based on user-given criteria, such as servings, calories, or grams of protein.</small>
+                                <br />
+                                <br />
+                                <p>Also Looks up foods with the USDA's food database API to quickly get scaled portion sizes.</p>
+                                <p></p>
+                            </div>
+
+                            <div className="d-flex justify-content-center d-flex p-2">
+                                <a href="https://tomatokei.herokuapp.com" target='blank'><button className="btn btn-primary mx-1 my-1"><i className="fa fa-globe"></i>&nbsp;Visit</button></a>
+                                <Link to="./tomatokei" ><button className="btn btn-success mx-1 my-1"><i className="fa fa-bar-chart"></i>&nbsp;Details</button></Link>
+                                <a href="https://github.com/bluegeartechnology/Tomatokei" target='blank'><button className="btn btn-secondary mx-1 my-1"><i className="fa fa-github"></i>&nbsp;GitHub</button></a>
+                            </div>
+
+                        </div>
                     </div>
 
 
