57020cdb39908e5a93ce386eae17c9502d5d211f
Josh <bluegeartechnology@gmail.com>
2021-02-21 23:16:30 -0500

    Added Kotlin to BarBelle's technologies list.

--- src/pages/projects/BarBelle.js
+++ src/pages/projects/BarBelle.js
@@ -492,6 +492,11 @@ const BarBelle = () => {
                             <img className='skillIcon col-12' src="android.png" alt="" />
                             <h3 className='text-center'>Android</h3>
                         </div>
+
+                        <div className='my-2 col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+                            <img className='skillIcon col-12' src="kotlin.png" alt="" />
+                            <h3 className='text-center'>Kotlin</h3>
+                        </div>
                     </div>
 
 
