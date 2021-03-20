cfac8deefa30b106b630a7d3a1236acec03456df
Josh <bluegeartechnology@gmail.com>
2021-03-20 08:16:01 -0400

    Continuing to convert JYST's project page to the ProjectPage component.

--- src/pages/projects/JYST.js
+++ src/pages/projects/JYST.js
@@ -11,9 +11,64 @@ function useHistory() {
 
 const JYST = () => {
 
-    let what = <></>
+    let what = <>
+        <p>
+            JYST is a web-scraper specifically for scraping local junkyard websites.
+            It primarily exists to enable part-flipping businesses to more easily find parts which can then be sold online.
+            It also contains tools for estimating profit margins of a trip to a given yard based on the cars found by the scrape.
+                    </p>
+
+        <p>
+            Secondarily, it enables consumers to search many yards for their cars simultaneously.
+            While many junkyards have inventory searches on their websites, and part interchanges exist, there is currently no convenient way for the average person to search many local yards for unpulled parts in a time-effective manner.
+            JYST solves this problem.
+                    </p>
+    </>
+
     let how = <></>
-    let tech = <></>
+
+    let tech = <>
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <img className='skillIcon col-12' src="html5.png" alt="" />
+            <h3 className='text-center'>HTML 5</h3>
+        </div>
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <img className='skillIcon col-12' src="css3.png" alt="" />
+            <h3 className='text-center'>CSS 3</h3>
+        </div>
+
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <img className='skillIcon col-12' src="javascript.png" alt="" />
+            <h3 className='text-center'>JavaScript</h3>
+        </div>
+
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <img className='skillIcon col-12' src="react.png" alt="" />
+            <h3 className='text-center'>ReactJS</h3>
+        </div>
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob row-column justify-content-between my-auto'>
+            <img className='skillIcon col-12' src="mongodb.png" alt="" />
+            <h3 className='text-center'>mongoDB</h3>
+        </div>
+
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <img className='skillIcon col-12' src="node.svg" alt="" />
+            <h3 className='text-center'>Node.js</h3>
+        </div>
+
+        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
+            <svg width="" height="100" viewBox="0 0 16 16" className="col-12 bi bi-bootstrap-fill" fill="#7952b3" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z" />
+            </svg>
+            <h3 className='text-center'>Bootstrap</h3>
+        </div>
+    </>
+
     let learned = <></>
 
 
@@ -73,60 +128,12 @@ const JYST = () => {
 
                 <div className='container col-11 col-sm-10 col-md-10 col-lg-8 col-xl-8 bg-dark rounded px-5 py-4 my-3'>
                     <h2>What is it?</h2>
-                    <p>
-                        JYST is a web-scraper specifically for scraping local junkyard websites.
-                        It primarily exists to enable part-flipping businesses to more easily find parts which can then be sold online.
-                        It also contains tools for estimating profit margins of a trip to a given yard based on the cars found by the scrape.
-                    </p>
 
-                    <p>
-                        Secondarily, it enables consumers to search many yards for their cars simultaneously.
-                        While many junkyards have inventory searches on their websites, and part interchanges exist, there is currently no convenient way for the average person to search many local yards for unpulled parts in a time-effective manner.
-                        JYST solves this problem.
-                    </p>
 
                     <h2>Technologies Used</h2>
                     <div className="text-light my-5">
 
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <img className='skillIcon col-12' src="html5.png" alt="" />
-                            <h3 className='text-center'>HTML 5</h3>
-                        </div>
 
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <img className='skillIcon col-12' src="css3.png" alt="" />
-                            <h3 className='text-center'>CSS 3</h3>
-                        </div>
-
-
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <img className='skillIcon col-12' src="javascript.png" alt="" />
-                            <h3 className='text-center'>JavaScript</h3>
-                        </div>
-
-
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <img className='skillIcon col-12' src="react.png" alt="" />
-                            <h3 className='text-center'>ReactJS</h3>
-                        </div>
-
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob row-column justify-content-between my-auto'>
-                            <img className='skillIcon col-12' src="mongodb.png" alt="" />
-                            <h3 className='text-center'>mongoDB</h3>
-                        </div>
-
-
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <img className='skillIcon col-12' src="node.svg" alt="" />
-                            <h3 className='text-center'>Node.js</h3>
-                        </div>
-
-                        <div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'>
-                            <svg width="" height="100" viewBox="0 0 16 16" className="col-12 bi bi-bootstrap-fill" fill="#7952b3" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z" />
-                            </svg>
-                            <h3 className='text-center'>Bootstrap</h3>
-                        </div>
                     </div>
 
 
