96886c03be77bab813231b592c1c5f58440a164e
Josh <bluegeartechnology@gmail.com>
2021-04-14 13:32:35 -0400

    Added scaling to BarBelle's Reverse Calculation button explanation text.

--- src/pages/projects/BarBelle.js
+++ src/pages/projects/BarBelle.js
@@ -376,7 +376,7 @@ const BarBelle = () => {
                     <div className="d-flex row justify-content-between">
                         <button className="btn btn-primary col-4 col-lg-2" onClick={(e) => calculateWeights(e)}>Calculate</button>
                         <button className="btn btn-warning col-6 col-md-4" onClick={(e) => setStandardOlympic(e)}>Reset Available Weights to <br />Standard 300lb Olympic Barbell Set</button>
-                        <button className="btn btn-success col-4 col-lg-2" onClick={(e) => reverseCalculateWeights(e)}>Reverse Calculation ("What's on this bar?") </button>
+                        <button className="btn btn-success col-4 col-lg-2" onClick={(e) => reverseCalculateWeights(e)}>Reverse Calculation<br /><span style={{ 'font-size': '0.85vw' }}>"What's on this bar?"</span> </button>
                     </div>
 
                     <div id="finalWeights" className='p-4 mx-auto col-10 text-center '></div>
