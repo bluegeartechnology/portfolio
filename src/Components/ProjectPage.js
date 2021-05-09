04f75220ac7869790960b9404a99675b718f0392
Josh <bluegeartechnology@gmail.com>
2021-05-09 15:56:57 -0400

    Continuing to convert badges to reusable snippets.

--- src/Components/ProjectPage.js
+++ src/Components/ProjectPage.js
@@ -18,8 +18,8 @@ function useHistory() {
             'nodejs': `<div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <img className='skillIcon col-12' src="node.svg" alt="" /> <h3 className='text-center'>Node.js</h3> </div>`,
             'bootstrap': `<div className='col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <svg width="" height="100" viewBox="0 0 16 16" class="col-12 bi bi-bootstrap-fill" fill="#7952b3" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z" /> </svg> <h3 className='text-center'>Bootstrap</h3> </div>`,
             'wordpress': `<div className='my-2 col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <img className='skillIcon col-12' src="wordpress.png" alt="" /> <h3 className='text-center text-dark'>WordPress</h3> </div>`,
-            'photoshop': `<div className='my-2 col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <img className='skillIcon col-12' src="photoshop.png" alt="" /> <h4 className='text-center text-dark'>Photoshop</h4> </div>`
-
+            'photoshop': `<div className='my-2 col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <img className='skillIcon col-12' src="photoshop.png" alt="" /> <h4 className='text-center text-dark'>Photoshop</h4> </div>`,
+            'premiere': `<div className='my-2 col-lg-2 col-md-3 col-sm-5 col-6 hvr-bob'> <img className='skillIcon col-12' src="premiere.png" alt="" /> <h4 className='text-center text-dark'>Premiere</h4> </div>`
         }
 
         //document.getElementById('techBadges').innerHTML = techBadges.html5 + techBadges.css3
